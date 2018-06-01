02d9a184d5a067b34a9bdc6efc094100597ef5b3
Logan Wood <lwood3499@gmail.com>
2018-05-31 23:17:44 -0500

    added health bars

--- section_4/myMonsterProject/monsterGame.js
+++ section_4/myMonsterProject/monsterGame.js
@@ -1,6 +1,45 @@
 let app = new Vue({
   el: '#app',
   data: {
-    message: 'hello Vue'
+    message: 'hello Vue',
+    playerNamesStyle: {
+      height: '150px',
+      padding: '0%'
+    },
+    playerContainer: {
+      marginLeft: '10%'
+    },
+    playerProgressBar: 100,
+    monsterContainer: {
+      marginRight: '10%'
+    },
+    monsterProgressBar: 100
+  },
+  computed: {
+    playerProgress: function() {
+      return {
+        backgroundColor: 'blue',
+        height: '20px',
+        width: this.playerProgressBar + '%'
+      };
+    },
+    monsterProgress: function() {
+      return {
+        backgroundColor: 'green',
+        height: '20px',
+        width: this.monsterProgressBar + '%'
+      };
+    }
+  },
+  methods: {
+    attack: function() {
+      if (this.playerProgressBar <= 0 || this.monsterProgressBar <= 0) {
+        this.playerProgressBar = 100;
+        this.monsterProgressBar = 100;
+      } else {
+        this.playerProgressBar -= 5;
+        this.monsterProgressBar -= 6;
+      }
+    }
   }
 });
